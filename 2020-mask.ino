091ae0f7aedd8d1acd32326e43b11d2955265f0f
Maggie <infosecmags@gmail.com>
2020-10-12 17:44:50 -0700

    added rainbow welcome banner

--- 2020-mask.ino
+++ 2020-mask.ino
@@ -92,12 +92,11 @@ const PROGMEM uint8_t mouth_smile[8][8] = {
     {0,0,0,0,0,0,0,0}
 };
 
-uint16_t palette[8] = {};
+uint16_t palette[10] = {};
 Adafruit_NeoMatrix matrix = Adafruit_NeoMatrix(8, 8, LEDPIN,
   NEO_MATRIX_TOP     + NEO_MATRIX_RIGHT +
-  NEO_MATRIX_COLUMNS    + NEO_MATRIX_ZIGZAG,
+  NEO_MATRIX_ROWS    + NEO_MATRIX_ZIGZAG,
   NEO_GRB            + NEO_KHZ800);
-
 
 void drawImage(short image_addr){
     for(int x = 0; x<8; x++){
@@ -121,6 +120,9 @@ int previous = LOW;    // the previous reading from the input pin
 long time = 0;         // the last time the output pin was toggled
 long debounce = 200;   // the debounce time, increase if the output flickers
 
+bool banner = true;
+int x    = matrix.width();
+
 void setup() {
   //LEDPIN connects to 8x8 matrix
     pinMode(LEDPIN, OUTPUT);
@@ -138,24 +140,24 @@ void setup() {
   //SWITCH is pushbutton input with internal pullup so defaults to high
     pinMode(SWITCH, INPUT_PULLUP);
 
-      
+    
     matrix.begin();
 
-    palette[0] = matrix.Color(0,0,0);
-    palette[1] = matrix.Color(B,0,0);
-    palette[2] = matrix.Color(0,B,0);
-    palette[3] = matrix.Color(0,0,B);
-    palette[4] = matrix.Color(0,B,B);
-    palette[5] = matrix.Color(B,0,B);
-    palette[6] = matrix.Color(0,B,0);
-    palette[7] = matrix.Color(B,B,B);
+    palette[0] = matrix.Color(0,0,0);   // black
+    palette[1] = matrix.Color(B,0,0);   // red
+    palette[2] = matrix.Color(B,0,B/2); // pink
+    palette[3] = matrix.Color(0,0,B);   // blue
+    palette[4] = matrix.Color(0,B,B);   // aqua
+    palette[5] = matrix.Color(B,0,B);   // purple
+    palette[6] = matrix.Color(0,B,0);   // green
+    palette[7] = matrix.Color(B,B,0);   // yellow
+    palette[8] = matrix.Color(B,B/2,0); // orange
+    palette[9] = matrix.Color(B,B,B);   // white
 
 //    matrix.setBrightness(BRIGHTNESS);
 
 
     Serial.begin(9600);
-
-  //TODO: add a welcome/thank you message at startup
 }
 
 float vol = 0;
@@ -176,52 +178,85 @@ void loop() {
   digitalWrite(MICVCC, state);     // button toggles mic on & off
   previous = reading;
   if(state == LOW) {
-    matrix.clear();                //if low power mode, clear display
+    matrix.clear();                // if low power mode, clear display
     matrix.show();
+    banner = true;                 // reset welcome banner
   }
-  else {                           //resume normal operation:
-    float nvol = 0;
-    int previous_peak = -1;
-    
-    for (int i = 0; i<samples; i++){
-        auto analog = analogRead(MICPIN);
-        auto micline = abs(analog - 512);
-
-        nvol = max(micline, nvol);
-    }
-
-    vol = (nvol + 1.0*vol)/2.0;
-
-    if(nvol > 200){
-        pop_detection += 1;
-        if(pop_detection > 2) {
-            smiling = false;
-            last_face = millis();
-        }
-    } else {
-        if(pop_detection > 0 && pop_detection <= 2) {
-            if(millis() > last_face + 500){
-                smiling = true;
-                smiletimer = millis() + 2000;
-            }
-        }
-        pop_detection = 0;
-    }
-
-    if(millis() > smiletimer) smiling = false;
-
-    if(smiling){
-        drawImage(mouth_smile);
-    } else if(vol < 200){
-        drawImage(mouth_0);
-    } else if(vol < 250){
-        drawImage(mouth_1);
-    } else if(vol < 350){
-        drawImage(mouth_2);
-    } else if(vol < 450){
-        drawImage(mouth_3);
-    } else {
-        drawImage(mouth_4);
+  else {                           // resume normal operation:
+    if(banner == true) {           // print welcome banner upon reset
+      matrix.fillScreen(0);
+      matrix.setCursor(x, 0);
+      matrix.setTextColor(palette[2]);
+      matrix.print(F("B"));
+      matrix.setTextColor(palette[5]);
+      matrix.print(F("S"));
+      matrix.setTextColor(palette[3]);
+      matrix.print(F("i"));
+      matrix.setTextColor(palette[4]);
+      matrix.print(F("d"));
+      matrix.setTextColor(palette[6]); 
+      matrix.print(F("e"));
+      matrix.setTextColor(palette[7]);
+      matrix.print(F("s"));
+      matrix.setTextColor(palette[8]); 
+      matrix.print(F("P"));
+      matrix.setTextColor(palette[1]);
+      matrix.print(F("D"));
+      matrix.setTextColor(palette[9]); 
+      matrix.print(F("X"));
+      if(--x < -60) {
+        x = matrix.width();
+      }
+      if(x == -60) {
+        banner = false;
+      }
+        matrix.show();
+        delay(100);
+      }
+    else {
+      float nvol = 0;
+      int previous_peak = -1;
+      
+      for (int i = 0; i<samples; i++){
+          auto analog = analogRead(MICPIN);
+          auto micline = abs(analog - 512);
+  
+          nvol = max(micline, nvol);
+      }
+  
+      vol = (nvol + 1.0*vol)/2.0;
+  
+      if(nvol > 200){
+          pop_detection += 1;
+          if(pop_detection > 2) {
+              smiling = false;
+              last_face = millis();
+          }
+      } else {
+          if(pop_detection > 0 && pop_detection <= 2) {
+              if(millis() > last_face + 500){
+                  smiling = true;
+                  smiletimer = millis() + 2000;
+              }
+          }
+          pop_detection = 0;
+      }
+  
+      if(millis() > smiletimer) smiling = false;
+  
+      if(smiling){
+          drawImage(mouth_smile);
+      } else if(vol < 200){
+          drawImage(mouth_0);
+      } else if(vol < 250){
+          drawImage(mouth_1);
+      } else if(vol < 350){
+          drawImage(mouth_2);
+      } else if(vol < 450){
+          drawImage(mouth_3);
+      } else {
+          drawImage(mouth_4);
+      }
     }
   }
 } 
